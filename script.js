fe50ff40c718b22708ff1e89de14a292f0b15135
MathildeCornelis <mathilde.cornelis26@gmail.com>
2022-12-10 18:35:39 +0100

    HTML + SCSS + début JS

---
+++ script.js
@@ -0,0 +1,29 @@
+let calc = `
+    <div class="result">
+        <p class="resultat">ejejeje</p>
+    </div>
+    <div class="touches">
+        <button>(</button>
+        <button>)</button>
+        <button>%</button>
+        <button>AC</button>
+        <button class='nbre'>7</button>
+        <button class='nbre'>8</button>
+        <button class='nbre'>9</button>
+        <button>÷</button>
+        <button class='nbre'>4</button>
+        <button class='nbre'>5</button>
+        <button class='nbre'>6</button>
+        <button>×</button>
+        <button class='nbre'>1</button>
+        <button class='nbre'>2</button>
+        <button class='nbre'>3</button>
+        <button>-</button>
+        <button class='nbre'>0</button>
+        <button>.</button>
+        <button>=</button>
+        <button>+</button>
+    </div>
+    `;
+let nombre = document.querySelectorAll('.nbre'); 
+console.log(nombre);
